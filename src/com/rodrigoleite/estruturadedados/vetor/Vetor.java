f967dc65a6c8cfff73b00450c45c828cc66736af
Rodrigo Leite <dev.rodrigosleite@gmail.com>
2026-01-27 09:30:43 -0300

    Primeiro Commit - inicializando projeto

---
+++ src/com/rodrigoleite/estruturadedados/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.rodrigoleite.estruturadedados.vetor;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
